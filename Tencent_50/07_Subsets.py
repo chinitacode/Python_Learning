497dfbc9c54e545da0a27b5bf48b0fbfc1790d1f
chinitacode <ziyu_zhou_victoria@163.com>
2019-08-11 19:57:16 +0800

    complete task6

---
+++ Tencent_50/07_Subsets.py
@@ -0,0 +1,42 @@
+'''
+# 78. Subsets
+
+1# Use recursion:
+'''
+def subset(nums):
+    '''
+    Runtime: 16 ms, faster than 90.16% of Python online submissions for Subsets.
+    Memory Usage: 12.1 MB, less than 22.03% of Python online submissions for Subsets.
+    '''
+    if len(nums) == 0:
+        return [[]]
+    res = subset(nums[:-1])
+    return res + [e + [nums[-1]] for e in res]
+
+#Use only iteration:
+def subsets2(nums):
+    res = [[]]
+    for num in nums:
+        res += [e + [num] for e in res]
+    return res
+
+def subsets(nums):
+    result = [[]]
+    for num in nums:
+        for e in result[:]:
+            x = e[:]
+            x.append(num)
+            result.append(x)
+    return result
+
+#Use backtracking
+def subsets_bk(nums):
+    lst,result = [],[]
+    def helper(result,lst,nums,pos):
+        result.append(lst[:])
+        for i in range(pos,len(nums)):
+            lst.append(nums[i])
+            helper(result,lst,nums,i+1)
+            lst.pop()
+    helper(result,lst,nums,0)
+    return result
