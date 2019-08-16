09175929fc83019f0524187716dd6d29a6b220e3
chinitacode <ziyu_zhou_victoria@163.com>
2019-08-16 19:38:42 +0800

    update two_sum

--- Algos/Task4_practice.py
+++ Algos/Task4_practice.py
@@ -11,19 +11,25 @@ String to Integer (atoi)（字符串转换整数 (atoi)）[作为可选]
 '''
 #1. Two Sum (Easy)
 # using hash O(N)
-    def twoSum(self, num, target):
-        map = {}
-        for i in range(len(num)):
-            if num[i] not in map:
-                map[target - num[i]] = i + 1
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        wanted_nums = {}
+        for i in range(len(nums)):
+            # check if the left value is in wanted_nums(if num is the left value)
+            if nums[i] in wanted_nums:
+                # return the index of the first value and the current index
+                return [wanted_nums[nums[i]], i]
             else:
-                return map[num[i]], i + 1
+                # Save each left value of tatget - num as key in wanted_nums
+                # before return, including negative values
+                wanted_nums[target - nums[i]] = i
+        return [-1, -1]
 
-        return -1, -1
 
-"""
-This way the number of iterations is the smallest because all wanted numbers are stored in a memory.
-"""
 # Or:
     def twoSum(self, nums, target):
         d = {}
@@ -33,15 +39,4 @@ This way the number of iterations is the smallest because all wanted numbers are
                 return [d[m], i]
             else:
                 d[n] = i
-
-#Or:
-class Solution(object):
-    def twoSum(self, nums, target):
-        if len(nums) <= 1:
-            return False
-        buff_dict = {}
-        for i in range(len(nums)):
-            if nums[i] in buff_dict:
-                return [buff_dict[nums[i]], i]
-            else:
-                buff_dict[target - nums[i]] = i
+        return [-1, -1]
