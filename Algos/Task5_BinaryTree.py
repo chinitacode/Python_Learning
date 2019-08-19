4aef49d66644d5ef8fa9f771084f9aed526321f9
chinitacode <ziyu_zhou_victoria@163.com>
2019-08-19 23:29:05 +0800

    update

--- Algos/Task5_BinaryTree.py
+++ Algos/Task5_BinaryTree.py
@@ -1,10 +1,13 @@
 '''
+---------------------------------------------------------------------
 【二叉树】
-实现一个二叉查找树，并且支持插入、删除、查找操作
-实现查找二叉查找树中某个节点的后继、前驱节点
-实现二叉树前、中、后序以及按层遍历
-并完成leetcode上的验证二叉搜索树(98)及二叉树层次遍历(102,107)！（选做）（保留往期第四天任务）
+1.实现一个二叉查找树，并且支持插入、删除、查找操作
+2.实现查找二叉查找树中某个节点的后继、前驱节点
+3.实现二叉树前、中、后序以及按层遍历
+4.并完成leetcode上的验证二叉搜索树(98)及二叉树层次遍历(102,107)！（选做）（保留往期第四天任务）
+---------------------------------------------------------------------
 '''
+#1.
 class Node:
     __slots__ = '_item' , '_left' , '_right'
 
@@ -14,8 +17,6 @@ class Node:
         self._right = right
 
 class BinarySearchTree:
-
-
     def __init__ (self, root=None):
         self._root = root
 
@@ -32,7 +33,6 @@ class BinarySearchTree:
             return self.__get(node._left, key)
         else:
             return self.__get(node._right, key)
-
 
     # add methods
     def add(self, value):
@@ -117,3 +117,74 @@ class BinarySearchTree:
         self._print_postorder(node._left)
         self._print_postorder(node._right)
         print ('[', node._item, ']', end = " ")
+
+
+'''
+###### 2.遍历 ######
+--------------------------------------------------------------------
+### 前/先序遍历（pre-order traversal）:
+    先（打印）parent,再（打印）child
+    根结点 ---> 左子树 ---> 右子树
+
+                   1
+                /     \
+              /         \
+             2           7
+           /   \        /  \
+          3     4      8    9
+              /   \
+             5     6
+
+        Algorithm preOrder(v): #v--vertex,节点
+            visit(v)
+            for each child w of v:
+                preOrder(w)
+
+---------------------------------------------------------------------
+### 后序遍历（post-order traversal）:
+    先（打印）child,再（打印）parent
+    左子树 ---> 右子树 ---> 根结点
+
+                   9
+                /     \
+              /         \
+             5           8
+           /   \        /  \
+          1     4      6    7
+              /   \
+             2     3
+
+Algorithm postOrder(v):
+    for each child w of v:
+        postOrder(w)
+    visit(v)
+---------------------------------------------------------------------
+### 中序遍历/顺序遍历(in-order traversal)
+    左子树---> 根结点 ---> 右子树
+    *在二叉树的顺序遍历中，常常会发生先遇到的节点到后面再访问的情况，
+    这和先进后出的栈的结构很相似，因此在非递归的实现方法中，我们最常使用的数据结构就是栈。
+
+                   6
+                /     \
+              /         \
+             2           8
+           /   \        /  \
+          1     4      7    9
+              /   \
+             3     5
+
+Algorithm inOrder(v):
+    if hasLeft(v):
+        inOrder(left(v))
+    visit(v)
+    if hasRight(v):
+        inOrder(right(v))
+---------------------------------------------------------------------
+### 层次遍历
+
+
+---------------------------------------------------------------------
+
+
+
+'''
