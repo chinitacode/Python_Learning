a81940eb5c99a4a04a13f00121294ef873530815
chinitacode <ziyu_zhou_victoria@163.com>
2019-08-21 22:15:48 +0800

    update

--- Algos/Task5_Practice.py
+++ Algos/Task5_Practice.py
@@ -290,3 +290,151 @@ class Solution(object):
             if curr.right:
                 queue.append((curr.right, val+1))
         return val
+
+'''
+111. Minimum Depth of Binary Tree
+Runtime: 20 ms, faster than 99.58% of Python online submissions for Minimum Depth of Binary Tree.
+Memory Usage: 15 MB, less than 15.38% of Python online submissions for Minimum Depth of Binary Tree.
+'''
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        if not root.left:
+            return 1 + self.minDepth(root.right)
+        elif not root.right:
+            return 1 + self.minDepth(root.left)
+        else:
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+# Or:(slower by 50%)
+class Solution:
+    # @param root, a tree node
+    # @return an integer
+    def minDepth(self, root):
+        if root == None:
+            return 0
+        if root.left==None or root.right==None:
+            return self.minDepth(root.left)+self.minDepth(root.right)+1
+        return min(self.minDepth(root.right),self.minDepth(root.left))+1
+
+# Or: add the smaller one of the child depths - except if that's zero, then add the larger one
+def minDepth(self, root):
+    if not root: return 0
+    d = map(self.minDepth, (root.left, root.right))
+    return 1 + (min(d) or max(d))
+
+def minDepth(self, root):
+    if not root: return 0
+    d, D = sorted(map(self.minDepth, (root.left, root.right)))
+    return 1 + (d or D)
+
+'''
+Iterative solution using queue(层次遍历)
+Runtime: 28 ms, faster than 90.80% of Python online submissions for Minimum Depth of Binary Tree.
+Memory Usage: 14.6 MB, less than 82.05% of Python online submissions for Minimum Depth of Binary Tree.
+'''
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root: return 0
+        queue = [(root, 1)]
+        while True:
+            curr, val = queue.pop(0)
+            if not curr.left and not curr.right:
+                return val
+            if curr.left:
+                queue.append((curr.left, val+1))
+            if curr.right:
+                queue.append((curr.right, val+1))
+
+'''
+110. Balanced Binary Tree
+Brute Froce: Recursive, O(nlgn)
+Runtime: 52 ms, faster than 39.13% of Python online submissions for Balanced Binary Tree.
+Memory Usage: 17.3 MB, less than 15.63% of Python online submissions for Balanced Binary Tree.
+'''
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None: return True
+        def maxx(node):
+            if node is None: return 0
+            return 1 + max(maxx(node.left), maxx(node.right))
+        #O(n)
+        if abs(maxx(root.left) - maxx(root.right)) > 1: return False
+        #O(lgn)
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+'''
+Recursive method with global variable
+其实我们在处理完get_height这个高度的时候，我们完全可以在检查每个节点高度并且返回的同时，
+记录左右差是否已经超过1，只要有一个节点超过1，那么直接返回False即可，
+因此我们只需要在外围设立一个全球变量记录True和False，在调用get_height的时候，内置代码里加入对左右高度的判定即可
+O(n)
+'''
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        self.isBalanced = True
+        self.getHeight(root)
+        return self.isBalanced
+
+
+    def getHeight(self, root):
+        if not root: return 0
+        left = self.getHeight(root.left)
+        right = self.getHeight(root.right)
+        if abs(left - right) > 1:
+            self.isBalanced = False
+        return max(left, right) + 1
+
+
+'''
+Method3: '-1'
+O(N)
+最后Leetcode上有一种-1的方法，其实就是上面这种方法的一种延伸。
+如果左右两边出现了高度差高于1的情况，直接返回-1，这个-1怎么来的？因为高度不可能为负数，-1其实就是一种True/False的表达。
+那么在实现上，我们只要对get_height每次返回前做一个判定即可
+'''
+class Solution(object):
+    def isBalanced(self, root):
+        height = self.get_height(root)
+        return height != -1
+
+
+    def get_height(self, root):
+        if not root: return 0
+        left = self.get_height(root.left)
+        right = self.get_height(root.right)
+        if left == -1 or right == -1 : return -1
+        if abs(left - right) > 1:  return -1
+        return max(left, right) + 1
+
+#Or:
+class Solution(object):
+    def isBalanced(self, root):
+
+        def check(root):
+            if root is None:
+                return 0
+            left  = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+
+        return check(root) != -1
