91581c647500e5b7b2d4e8b5799eb3b591fe4007
chinitacode <ziyu_zhou_victoria@163.com>
2019-10-04 22:13:26 +0200

    complete task 25

--- Tencent_50/26_SpiralMatrix.py
+++ Tencent_50/26_SpiralMatrix.py
@@ -1,6 +1,7 @@
 '''
-54. Spiral Matrix
-Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+54. Spiral Matrix [Medium]
+Given a matrix of m x n elements (m rows, n columns),
+return all elements of the matrix in spiral order.
 
 Example 1:
 Input:
@@ -21,65 +22,111 @@ Input:
 Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 
 [Analysis]
-The path that the spiral makes will turn clockwise whenever it would go out of bounds or into a cell that was previously visited.
+The path that the spiral makes will turn clockwise whenever it would go out of bounds
+or into a cell that was previously visited.
 
 
-Method1:
-Let the array have R rows and C columns. 
-seen[r][c] denotes that the cell on the r-th row and c-th column was previously visited. 
-Our current position is (r, c), facing direction di, and we want to visit R x C total cells.
+[Method 1]: Traversing each element by changing directions
+Let the array have m rows and n columns.
+seen[r][c] denotes that the cell on the r-th row and c-th column was previously visited.
+Our current position is (row, col), facing direction di, and we want to visit m x n total cells.
 
-As we move through the matrix, our candidate's next position is (cr, cc). 
-If the candidate is in the bounds of the matrix and unseen, then it becomes our next position; 
+As we move through the matrix, our candidate's next position is (r, c).
+If the candidate is in the bounds of the matrix and unseen, then it becomes our next position;
 otherwise, our next position is the one after performing a clockwise turn.
 
-[Time Complexity]: O(N), where NN is the total number of elements in the input matrix. 
+[Time Complexity]: O(N), where N is the total number of elements in the input matrix.
 We add every element in the matrix to our final answer.
-[Space Complexity]: O(N), the information stored in seen and in ans. 
+[Space Complexity]: O(N), the information stored in seen and in ans.
 
-
+Runtime: 36 ms, faster than 73.43% of Python3 online submissions for Spiral Matrix.
+Memory Usage: 13.6 MB, less than 8.70% of Python3 online submissions for Spiral Matrix.
 '''
-class Solution(object):
-    def spiralOrder(self, matrix):
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         if not matrix: return []
-        R, C = len(matrix), len(matrix[0])
-        seen = [[False] * C for _ in matrix]
+        m, n = len(matrix), len(matrix[0])
+        di_r = [0, 1, 0, -1]
+        di_c = [1, 0, -1, 0]
+        seen = [[0]*n for row in matrix]
         ans = []
-        dr = [0, 1, 0, -1]
-        dc = [1, 0, -1, 0]
-        r = c = di = 0
-        for _ in range(R * C):
-            ans.append(matrix[r][c])
-            seen[r][c] = True
-            cr, cc = r + dr[di], c + dc[di]
-            if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
-                r, c = cr, cc
+        row = col = di = 0
+        for cell in range(m*n):
+            ans.append(matrix[row][col])
+            seen[row][col] = 1
+            r, c = row + di_r[di], col + di_c[di]
+            if 0 <= r < m and 0 <= c < n and not seen[r][c]:
+                row, col = r, c
             else:
-                di = (di + 1) % 4
-                r, c = r + dr[di], c + dc[di]
+                di = (di+1) % 4
+                row, col = row + di_r[di], col + di_c[di]
+        return ans
+'''
+#or [Optimization]:
+Runtime: 36 ms, faster than 73.43% of Python3 online submissions for Spiral Matrix.
+Memory Usage: 13.8 MB, less than 8.70% of Python3 online submissions for Spiral Matrix.
+'''
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+        if not matrix:
+            return ans
+        i, j, di, dj = 0, 0, 0, 1
+        m, n = len(matrix), len(matrix[0])
+        for cell in range(m * n):
+            ans.append(matrix[i][j])
+            matrix[i][j] = ''
+            if matrix[(i + di) % m][(j + dj) % n] == '':
+                di, dj = dj, -di
+            i += di
+            j += dj
         return ans
 
-#or:
-def spiralOrder(self, matrix):
-    res = []
-    if not matrix:
-        return []
-    i,j,di,dj = 0,0,0,1
-    m, n = len(matrix),len(matrix[0])
-    for v in xrange(m * n):
-        res.append(matrix[i][j])
-        matrix[i][j] = ''
-        if matrix[(i+di)%m][(j+dj)%n] == '':
-            di, dj = dj, -di
-        i += di
-        j += dj
-        
-    return res
+'''
+[Method 2]: Peeling off the matrix layer by layer.
+Time: O(N), Space: O(N)
+Runtime: 36 ms, faster than 73.43% of Python3 online submissions for Spiral Matrix.
+Memory Usage: 13.6 MB, less than 8.70% of Python3 online submissions for Spiral Matrix.
+'''
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+        if not matrix:
+            return ans
+        l, t, b, r = 0, 0, len(matrix)-1, len(matrix[0])-1
+        while l < r and t < b:
+            #Peeling the top
+            for i in range(l, r):
+                ans.append(matrix[l][i])
+
+            #Peeling the right
+            for i in range(t, b):
+                ans.append(matrix[i][r])
+            #Peeling the bottom
+            for i in range(r, l, -1):
+                ans.append(matrix[b][i])
+            #Peeling the left
+            for i in range(b, t, -1):
+                ans.append(matrix[i][l])
+            b -= 1
+            t += 1
+            l += 1
+            r -= 1
+        #Adding the leftover in the middle
+        #Horizontal line left
+        if t == b:
+            for i in range(l, r+1):
+                ans.append(matrix[l][i])
+        #Vertical line left
+        elif l == r:
+            for i in range(t,b+1):
+                ans.append(matrix[i][l])
+        return ans
+
 
 '''
-Method2:
-Peel the the matrix layer by layer.
-Mutating the matrix, if this is not allowed, we can make a deep copy of the matrix first. 
+[Method 3]: Peeling off the matrix by pop(),
+if this is not allowed, we can make a deep copy of the matrix first.
 And of course it comes with the additional memory usage.
 
 Time: O(m(m+n)) because of pop(0)
@@ -100,3 +147,57 @@ class Solution:
                 for row in matrix[::-1]:
                     ret.append(row.pop(0))
         return ret
+
+
+'''
+[Method 4]: Python One-liner
+[Note]:
+if matrix = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+Then:
+>>> [*matrix.pop(0)]
+[1, 2, 3]
+>>> matrix
+[[4, 5, 6], [7, 8, 9]]
+>>>
+
+[Visualization]
+Here's how the matrix changes by always extracting the first row
+and rotating the remaining matrix counter-clockwise:
+
+    |1 2 3|      |6 9|      |8 7|      |4|  =>  |5|  =>  ||
+    |4 5 6|  =>  |5 8|  =>  |5 4|  =>  |5|
+    |7 8 9|      |4 7|
+
+Now look at the first rows we extracted:
+
+    |1 2 3|      |6 9|      |8 7|      |4|      |5|
+
+Those concatenated are the desired result.
+
+[Another visualization]
+  spiral_order([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+
+= [1, 2, 3] + spiral_order([[6, 9],
+                            [5, 8],
+                            [4, 7]])
+
+= [1, 2, 3] + [6, 9] + spiral_order([[8, 7],
+                                     [5, 4]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + spiral_order([[4],
+                                              [5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + spiral_order([[5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + spiral_order([])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + []
+
+= [1, 2, 3, 6, 9, 8, 7, 4, 5]
+'''
+def spiralOrder(self, matrix):
+    return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
