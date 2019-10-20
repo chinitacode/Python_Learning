e19ea17797277e212ee41bfbf3ec3410e449e993
chinitacode <ziyu_zhou_victoria@163.com>
2019-10-20 23:47:20 +0200

    complete task 22

--- Tencent_50/23_3Sum Closest.py
+++ Tencent_50/23_3Sum Closest.py
@@ -9,76 +9,38 @@ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
 
 
-Two Pointer Solution: O(N^2)
+[Method 1]: Two Pointer
+Need to introduce a variable representing the closest sum (named res),
+first initiated by a very giant number float('inf')
+and the current sum (named Sum) of the current three numbers.
+Each time we compare the difference between Sum and target,
+and the difference between the previously closest sum res and target,
+to decide whether to update the closest sum res.
+Return res after the iteration of the first number start is finished (index from 0 to N-3)
 
-1.We can use the two pointer method to reduce complexity to O(N^2).
-We begin by sorting the array.
-
-2.Now we use three indices i,j and k.
-We iterate i from 0 to N (actually till N-2 is fine).
-We initialize j to i+1 and k to N-1.
-
-3.Now we compute curr_sum = nums[i]+nums[j]+nums[k].
-If this equals target, we have the closest sum.
-
-4.Otherwise update closest_sum using the rule abs(curr_sum-target) < abs(closest_sum-target).
-
-5.Now what if curr_sum is less than target.
-Should we test (nums[i]+nums[j]+nums[k-1]), (nums[i]+nums[j]+nums[k-2]), (nums[i]+nums[j]+nums[k-3]) ?
-The answer is NO. All of these triplets will be less than curr_sum.
-And curr_sum is less than target - so there is no point testing these triplets.
-We must move forward by advancing j to j + 1 in the hope to get a larger triplet.
-This is the main intuition in this problem.
-
-6.You can visualize (6) by thinking all possible triplet sums sorted and arranged on a number line.
-When you find a curr_sum less than target, you increase curr_sum by increasing j.
-When you find a curr_sum less more than target, you reduce curr_sum by reducing k
+[Time]: O(N^2)
+[Space]: O(1)
+Runtime: 192 ms, faster than 27.21% of Python3 online submissions for 3Sum Closest.
+Memory Usage: 13.6 MB, less than 5.41% of Python3 online submissions for 3Sum Closest.
 '''
-class Solution(object):
-    def threeSumClosest(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        nums.sort()
-        closest_sum = 2**31-1
-        for i in range(len(nums)):
-            j,k = i+1, len(nums)-1
-            while j<k:
-                curr_sum = nums[i] + nums[j] + nums[k]
-                if curr_sum == target:
-                    return curr_sum
-                if abs(curr_sum-target) < abs(closest_sum-target):
-                    closest_sum = curr_sum
-                if curr_sum < target:
-                    j = j+1
-                else:
-                    k = k-1
-        return closest_sum
-
-#Or:
 class Solution:
-    # @return an integer
-    def threeSumClosest(self, num, target):
-        num.sort()
-        result = num[0] + num[1] + num[2]
-        for i in range(len(num) - 2):
-            j, k = i+1, len(num) - 1
-            while j < k:
-                sum = num[i] + num[j] + num[k]
-                if sum == target:
-                    return sum
-
-                if abs(sum - target) < abs(result - target):
-                    result = sum
-
-                if sum < target:
-                    j += 1
-                elif sum > target:
-                    k -= 1
-
-        return result
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if len(nums) < 3: return
+        nums.sort()
+        res = float('inf')
+        for start in range(len(nums)-2):
+            l, r = start+1, len(nums)-1
+            while l<r:
+                tmp = [nums[start], nums[l], nums[r]]
+                Sum = sum(tmp)
+                if Sum - target == 0: return Sum
+                elif Sum - target < 0:
+                    l += 1
+                else:
+                    r -= 1
+                if abs(Sum-target) < abs(res-target):
+                    res = Sum
+        return res
 
 '''
 Brute-force: O(N^3)
