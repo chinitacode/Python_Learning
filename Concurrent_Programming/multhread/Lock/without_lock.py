c296b8bdb06895efbb0266c21a3ac69dcd9806f2
chinitacode <ziyu_zhou_victoria@163.com>
2020-01-12 23:54:20 +0800

    update

--- Concurrent_Programming/multhread/Lock/without_lock.py
+++ Concurrent_Programming/multhread/Lock/without_lock.py
@@ -26,5 +26,26 @@ if __name__== '__main__':
     t2.join()
     '''
     Results:
-    
+    job1: 1job2: 11
+
+    job1: 12job2: 22
+
+    job1: 23job2: 33
+
+    job1: 34job2: 44
+
+    job1: 45job2: 55
+
+    job1: 56job2: 66
+
+    job1: 67job2: 77
+
+    job1: 78job2: 88
+
+    job1: 89job2: 99
+
+    job1: 100job2: 110
+
+    可以看出结果很混乱，因为t1和t2几乎同时开始，
+    t2.join()在t1.join()最后也只是使t2在t1结束后结束而已。
     '''
