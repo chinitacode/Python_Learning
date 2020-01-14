b8503359a74902d5132f0b352a83bb53d312d1ee
chinitacode <ziyu_zhou_victoria@163.com>
2020-01-14 09:22:36 +0800

    add a self-difined thread class

--- Concurrent_Programming/multhread/Add_thread/add_thread_by_class.py
+++ Concurrent_Programming/multhread/Add_thread/add_thread_by_class.py
@@ -1,7 +1,7 @@
 '''
 通过继承Thread类，并重写它的run()方法来创建线程：
 '''
-import threading
+import threading, time
 
 class MyThread(threading.Thread):
     def __init__(self, thread_name):
@@ -11,6 +11,34 @@ class MyThread(threading.Thread):
     def run(self):
         print("%s正在运行中......" % self.name)
 
+
+class Sayhi(threading.Thread):
+    def __init__(self,name):
+        super().__init__()
+        self.name = name
+    def run(self):
+        time.sleep(2)
+        print('%s say hello' % self.name)
+
+
 if __name__ == '__main__':
     for i in range(10):
         MyThread("thread-" + str(i)).start()
+    t = Sayhi('太白')
+    t.start()
+    print('主线程')
+    '''
+    【输出结果】:
+    thread-0正在运行中......
+    thread-1正在运行中......
+    thread-2正在运行中......
+    thread-3正在运行中......
+    thread-4正在运行中......
+    thread-5正在运行中......
+    thread-6正在运行中......
+    thread-7正在运行中......
+    thread-8正在运行中......
+    thread-9正在运行中......
+    主线程
+    太白 say hello
+    '''
